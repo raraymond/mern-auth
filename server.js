10cd84e76a292a21e80a29f324581fbd905be759
Ryan Raymond <ryan@Ryans-iMac.local>
2020-05-01 10:47:53 -0400

    created api endpoints and tested in postman for login and registeration

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-require('dotenv').config();
+const passport = require('passport');
+const users = require('./routes/api/users');
 
 const app = express();
 
@@ -14,12 +15,20 @@ app.use(
 app.use(bodyParser.json());
 
 //configure the database
-const db = process.env.DB;
+const db = require('./config/keys').mongoURI;
 //connect to mongoDB
 mongoose
     .connect(db, { useNewUrlParser: true })
     .then(() => console.log('MongoDB successfully connected'))
     .catch((err) => console.log(err));
+
+//passport middleware
+app.use(passport.initialize());
+//passport config
+require('./config/passport')(passport);
+
+//routes
+app.use('/api/users', users);
 
 const port = process.env.PORT || 5000; //use heroku if deployed theregit
 
